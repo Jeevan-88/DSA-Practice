69eac505dca2dc760bacec104c638457eeedd552
Jeevan Yadav <jeevanyadav2008@gmail.com>
2026-03-29 20:48:15 +0530

    Day-11: Binary Search problems + LeetCode 704 and 35

--- Day-11-BinarySearch/Problem31_SearchInsert.java
+++ Day-11-BinarySearch/Problem31_SearchInsert.java
@@ -1,37 +1,36 @@
+
 // Problem 30: Binary Search
 // Difficulty: Easy
 // Pattern: Binary Search
 // Date: Mar 18, 2026
 import java.util.Scanner;
 
-public class Problem30_BinarySearch {
-    public static void main(String[] args){
+public class Problem31_SearchInsert {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter array size: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
         System.out.println("Enter sorted elements:");
-        for(int i = 0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         System.out.print("Enter target element: ");
         int target = sc.nextInt();
         int left = 0;
         int right = arr.length - 1;
-        while(left <= right){
+        while (left <= right) {
             int mid = (left + right) / 2;
-            if(arr[mid] == target){
-                System.out.println("Element found at index:"+mid);
+            if (arr[mid] == target) {
+                System.out.println("Element found at index:" + mid);
                 return;
-            }
-            else if(arr[mid]<target){
+            } else if (arr[mid] < target) {
                 left = mid + 1;
 
-            }
-            else{
+            } else {
                 right = mid - 1;
             }
         }
-        System.out.println("Element not found in the array.");
+        System.out.println("Element to be inserted at index: " + left);
     }
 }
