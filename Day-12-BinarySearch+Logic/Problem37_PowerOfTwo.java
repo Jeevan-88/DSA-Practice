5408c4bd0cb7387b5f31a52093635135334feb5e
Jeevan Yadav <jeevanyadav2008@gmail.com>
2026-05-10 00:56:55 +0530

    Transitioning to 300-Problem Target & Strict Mentor Protocol

---
+++ Day-12-BinarySearch+Logic/Problem37_PowerOfTwo.java
@@ -0,0 +1,18 @@
+// Problem 37: Power of Two
+// LeetCode #231
+// Pattern: Bit Manipulation
+// Date: May 9, 2026 
+import java.util.*;
+
+public class Problem37_PowerOfTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        // TODO: Write logic to check if n is a power of two
+        // Can you do it in ONE line of code using Bitwise Operators?
+        
+        sc.close();
+    }
+}
